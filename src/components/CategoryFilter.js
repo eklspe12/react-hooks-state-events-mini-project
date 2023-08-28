b786c9441213dee6c17b912f37c4d6fe23827b29
Spencer <eklund.spencer@outlook.com>
2023-08-28 18:56:52 -0500

    Combined props with useState to create form

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({currentCategory, onCurrentCategory, categories}) {
+  const categoryButtons = categories.map((category) => {
+    const className = category === currentCategory ? "selected" : null;
+    return (
+      <button key ={category} className={className} onClick={() => onCurrentCategory(category)}>{category}</button>
+    )
+    })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
